af780ac1754faf2387e6b83460806daf206fcfb3
Dessmian <47385734+Dessmian@users.noreply.github.com>
2019-02-06 12:30:24 -0300

    Add files via upload

---
+++ 1-EntradaSalida/1-EntradaSalida/jsEntradaSalida-2.js
@@ -0,0 +1,9 @@
+/*Debemos lograr tomar un nombre con 'prompt' 
+y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+function Mostrar()
+{
+    var Nombre;
+    Nombre = prompt ("Ingrese su nombre");
+    alert ("Bienvenido "+Nombre);
+}
+
