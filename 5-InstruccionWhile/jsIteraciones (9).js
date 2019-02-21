b1aea82348bf2940647654cf7c3fa200f4d9428d
Dessmian <47385734+Dessmian@users.noreply.github.com>
2019-02-21 15:42:40 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -2,13 +2,31 @@ function Mostrar()
 {
 	var array_numero = [];
 	var contador = 0;
+	var mayor;
+	var menor;
 	
 	do{
-		array_numero[] = contador++; //parseFloat(prompt("Ingrese un numero"));
-		contaddor++;		
-		console.log(array_numero);		
-	}while( contador < 10);
+		array_numero[contador] = parseFloat(prompt("Ingrese un numero"));
+		console.log(array_numero);
+		if(contador == 0)
+		{
+			mayor=array_numero[contador];
+			menor=array_numero[contador];
+		}
+		if(array_numero[contador]>mayor)
+		{
+			mayor=array_numero[contador];
+		}
+		if(array_numero[contador]<menor)
+		{
+			menor=array_numero[contador];
+		}
+		contador++;
+		
+	}while( contador < 3);
+	alert(mayor);
+	alert(menor);
 
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCIÓN 
